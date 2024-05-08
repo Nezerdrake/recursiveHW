62c72f1cc7cc422b811002149e9b4f096f9b425d
Nezerdrake <nezerdrake@gmail.com>
2024-05-08 17:12:14 +0300

    добавлены решения на все задачи

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void Main()
+{
+    Console.Write("Введите значение M: ");
+    int M = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение N: ");
+    int N = int.Parse(Console.ReadLine());
+    if(N <= M)
+    {
+        int temp = M;
+        M = N;
+        N = temp;
+    }
+    PrintNaturalNumbers(M, N);
+}
+
+void PrintNaturalNumbers(int current, int N)
+{
+    if (current <= N)
+    {
+        Console.Write(current + " ");
+        PrintNaturalNumbers(current + 1, N);
+    }
+}
+
+Main();
---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿void Main()
+{
+    int[] myArray = { 10, 20, 30, 40, 50 };
+
+    Console.WriteLine("Элементы массива, начиная с конца:");
+    PrintArrayReverse(myArray, myArray.Length - 1);
+}
+
+void PrintArrayReverse(int[] arr, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(arr[index] + " ");
+        PrintArrayReverse(arr, index - 1);
+    }
+}
+
+Main();
